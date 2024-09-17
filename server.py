137cf616444d92fca16cce25b2bb67bacfcfb174
ShahidSandhu <sandhulive@gmail.com>
2024-09-17 14:48:37 -0400

    initial commit

--- server.py
+++ server.py
@@ -4,17 +4,26 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
+# Import libraries
+from flask import Flask, request
 
 #Initiate the flask app : TODO
+app = Flask(__name__)
+
 
 @app.route("/sentimentAnalyzer")
-def sent_analyzer():
+def sentiment_analyzer(text_to_analyse):
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
+    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    return response.text  # Return the response text from the API
+
 
 @app.route("/")
 def render_index_page():
@@ -26,3 +35,4 @@ def render_index_page():
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(debug=True)
