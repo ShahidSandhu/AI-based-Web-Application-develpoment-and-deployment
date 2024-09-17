33a3828d459b0a108f62990e5b5e32d24734de53
ShahidSandhu <sandhulive@gmail.com>
2024-09-17 15:02:02 -0400

    code completed

--- server.py
+++ server.py
@@ -2,27 +2,34 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
-# Import libraries
-from flask import Flask, request
 
-#Initiate the flask app : TODO
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+
+#Initiate the flask app
+app = Flask("Sentiment Analyzer")
 
 
 @app.route("/sentimentAnalyzer")
-def sentiment_analyzer(text_to_analyse):
+def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
-    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = sentiment_analyzer(text_to_analyze)
+
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    # Return a formatted string with the sentiment label and score
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 
 @app.route("/")
@@ -30,9 +37,10 @@ def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
     app.run(debug=True)
